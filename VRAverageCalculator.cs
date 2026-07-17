7a76aa909a30c4ac52c105386fa743dc8042807f
Nunchuk <49164897+ToastedOven@users.noreply.github.com>
2026-07-17 02:11:00 -0400

    refactor

--- VRAverageCalculator.cs
+++ VRAverageCalculator.cs
@@ -1,24 +1,26 @@
 using System;
 using System.Collections.Generic;
-using Godot;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using CounterTool;
+using Godot;
 using Godot.Collections;
 using ImageMagick;
 using OpenCvSharp;
-using FileAccess = System.IO.FileAccess;
 
 public partial class VRAverageCalculator : Node
 {
     public static List<int> currentMatchVRs = new();
     public static string myCurrentVR;
+    public static VRAverageCalculator instance;
+
     [Export] private Array<Button> vrButtons;
     [Export] private Button resetButton;
     [Export] private Label averageVR, myVR, racerCount;
-    public static VRAverageCalculator instance;
+    
+    private int prevAverageVR = -1;
 
     public override void _Ready()
     {
@@ -41,154 +43,132 @@ public partial class VRAverageCalculator : Node
     {
         currentMatchVRs.Clear();
         myCurrentVR = "null";
-        checkTimer = 2.5;
-        averageVR.Text = $"Average VR: null";
-        myVR.Text = $"My VR: null";
-        racerCount.Text = $"Real Player Count: null";
+        averageVR.Text = "Average VR: null";
+        myVR.Text = "My VR: null";
+        racerCount.Text = "Real Player Count: null";
     }
 
-    private double checkTimer = 2.5;
-    public override void _Process(double delta)
+    public async void ProcessVR()
     {
-        // checkTimer -= delta;
-        if (checkTimer < 0)
-        {
-            checkTimer = 2.5;
-            ProcessVR();
-        }
-    }
+        averageVR.Text = "Average VR: checking...";
+        myVR.Text = "My VR: checking...";
+        racerCount.Text = "Real Player Count: checking...";
 
-    private string leftBounds = $"{61}:{900}:{448}:{100}";
-    private string rightBounds = $"{61}:{900}:{951}:{100}";
-    private int prevAverageVR = -1;
-    async public void ProcessVR()
-    {
-        averageVR.Text = $"Average VR: checking...";
-        myVR.Text = $"My VR: checking...";
-        racerCount.Text = $"Real Player Count: checking...";
         await GetSourceImage("BaseImages/ligmaballs.tiff");
+
+        List<Task> cropTasks = new();
+
         for (int x = 0; x < 2; x++)
         {
             for (int y = 0; y < 12; y++)
             {
-                string thisBounds = $"{85}:{25}:{(x == 0 ? 440 : 940)}:{(110 + (y * 76))}";
-                await ProcessImageAsync(thisBounds, $"OcrImages/{x},{y}.tiff", 90, "BaseImages/ligmaballs.tiff");
-            }
-        }
-        for (int x = 0; x < 2; x++)
-        {
-            for (int y = 0; y < 12; y++)
-            {
-                string thisBounds = $"{85}:{25}:{(x == 0 ? 440 : 940)}:{(110 + (y * 76))}";
-                await ProcessImageAsync(thisBounds, $"OcrImages/myScore_{x},{y}.tiff", 200, "BaseImages/ligmaballs.tiff");
+                string bounds = $"{85}:{25}:{(x == 0 ? 440 : 940)}:{(110 + (y * 76))}";
+                cropTasks.Add(ProcessImageAsync(bounds, $"OcrImages/{x},{y}.tiff", 90, "BaseImages/ligmaballs.tiff"));
+                cropTasks.Add(ProcessImageAsync(bounds, $"OcrImages/myScore_{x},{y}.tiff", 200, "BaseImages/ligmaballs.tiff"));
             }
         }
 
-        List<int> allScores = new();
-        int myScore = 0;
-
-        int myX = -1, myY = -1;
+        await Task.WhenAll(cropTasks);
 
         List<Task<(int score, int x, int y)>> myScoreOcrTasks = new();
         List<Task<(int score, int x, int y)>> ocrTasks = new();
+
         for (int x = 0; x < 2; x++)
         {
             for (int y = 0; y < 12; y++)
             {
                 myScoreOcrTasks.Add(RunOcrForRegion($"OcrImages/myScore_{x},{y}.tiff", x, y));
-            }
-        }
-        
-        for (int x = 0; x < 2; x++)
-        {
-            for (int y = 0; y < 12; y++)
-            {
                 ocrTasks.Add(RunOcrForRegion($"OcrImages/{x},{y}.tiff", x, y));
             }
         }
-        await Task.WhenAll(myScoreOcrTasks.ToArray());
+
+        await Task.WhenAll(myScoreOcrTasks);
+        await Task.WhenAll(ocrTasks);
+
+        int myX = -1, myY = -1;
+        int myScore = 0;
+
         foreach (var task in myScoreOcrTasks)
         {
-            if (task.Result.score != 0)
+            var result = task.Result;
+            if (result.score != 0)
             {
-                myX = task.Result.x;
-                myY = task.Result.y;
-                myScore = task.Result.score;
+                myX = result.x;
+                myY = result.y;
+                myScore = result.score;
             }
         }
-        await Task.WhenAll(ocrTasks.ToArray());
+
+        List<int> allScores = new();
         foreach (var task in ocrTasks)
         {
-            if (task.Result.x == myX && task.Result.y == myY)
+            var result = task.Result;
+            if (result.x == myX && result.y == myY)
             {
                 allScores.Add(myScore);
             }
-            else if (task.Result.score != 0)
+            else if (result.score != 0)
             {
-                allScores.Add(task.Result.score);
+                allScores.Add(result.score);
             }
         }
-        
+
         if (allScores.Count == 0)
         {
             return;
         }
-        int totalScore = 0;
-        foreach (var score in allScores)
-        {
-            totalScore += score;
-        }
+        RecentTrackTracker.instance.timer = 0;
         if (myScore != 0)
         {
             myCurrentVR = $"{myScore}";
         }
         else
         {
-            myCurrentVR = $"Couldn't get my VR";
+            myCurrentVR = "Couldn't get my VR";
             Reset();
             return;
         }
-        int totalVR = 0;
-        foreach (var playerVR in allScores)
+
+        if (allScores.Any(score => score > 9999 || score < 1000))
         {
-            if (playerVR > 9999 || playerVR < 1000)
-            {
-                Reset();
-                return;
-            }
-            totalVR += playerVR;
-            // GD.Print($"VR: {playerVR}");
+            Reset();
+            return;
         }
+
+        int totalVR = allScores.Sum();
         currentMatchVRs = allScores;
-        averageVR.Text = $"Average VR: {totalVR / allScores.Count}";
+        
+        int average = totalVR / allScores.Count;
+        averageVR.Text = $"Average VR: {average}";
         myVR.Text = $"My VR: {myCurrentVR}";
         racerCount.Text = $"Real Player Count: {allScores.Count}";
-        // GD.Print($"Lobby count: {allScores.Count}, Average VR: {totalVR / allScores.Count}, Current VR: {myCurrentVR}");
-        if (totalVR / allScores.Count == prevAverageVR)
+
+        if (average == prevAverageVR)
         {
             prevAverageVR = -1;
-            checkTimer = 60;
         }
-        else
-        {
-            checkTimer = 2.5;
-        }
-        prevAverageVR = totalVR / allScores.Count;
+        
+        prevAverageVR = average;
     }
+
     public async Task GetSourceImage(string filename)
     {
-        using var capture = new VideoCapture(CameraSetup.currentCamera);
-        if (capture.IsOpened())
+        await Task.Run(() =>
         {
-            CameraSetup.ConfigureResolution(capture);
-            using var frame = new Mat();
-            capture.Read(frame);
-            CameraSetup.ApplyTransforms(frame);
-            using var newFrame = new Mat();
-            Cv2.Resize(frame, newFrame, new Size(1920, 1080));
-            Cv2.ImWrite(filename,newFrame);
-        }
+            using var capture = new VideoCapture(CameraSetup.currentCamera);
+            if (capture.IsOpened())
+            {
+                CameraSetup.ConfigureResolution(capture);
+                using var frame = new Mat();
+                capture.Read(frame);
+                CameraSetup.ApplyTransforms(frame);
+                using var newFrame = new Mat();
+                Cv2.Resize(frame, newFrame, new Size(1920, 1080));
+                Cv2.ImWrite(filename, newFrame);
+            }
+        });
     }
+
     public async Task ProcessImageAsync(string cropCoords, string filename, int threshold, string sourceFile)
     {
         var parts = cropCoords.Split(':');
@@ -202,23 +182,21 @@ public partial class VRAverageCalculator : Node
 
         await Task.Run(() =>
         {
-            using FileStream fs = new FileStream(sourceFile, FileMode.Open, FileAccess.Read);
-            using var image = new MagickImage(fs);
+            using var image = new MagickImage(sourceFile);
             image.Crop(new MagickGeometry(x, y, (uint)width, (uint)height));
-            
             image.ResetPage(); 
 
             if (threshold > 0)
             {
                 image.ColorType = ColorType.Grayscale;
-
-                double thresholdPercentage = ((double)threshold / 255.0) * 100.0;
+                double thresholdPercentage = (threshold / 255.0) * 100.0;
                 image.Threshold(new Percentage(thresholdPercentage));
             }
 
             image.Write(filename);
         });
     }
+
     private async Task<(int score, int x, int y)> RunOcrForRegion(string filename, int x, int y)
     {
         return await Task.Run(() =>
@@ -228,7 +206,8 @@ public partial class VRAverageCalculator : Node
                 FileName = "tesseract",
                 Arguments = $"{filename} stdout --psm 11 -c tessedit_char_whitelist=0123456789 --tessdata-dir {tessDataPath} -l eng2",
                 RedirectStandardOutput = true,
-                UseShellExecute = false, CreateNoWindow = true
+                UseShellExecute = false, 
+                CreateNoWindow = true
             };
 
             using (var p = Process.Start(tesseractInfo))
